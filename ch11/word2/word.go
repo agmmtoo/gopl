09ee21db307894995da4ff5b1b72095bb4528571
ammo <solidifyarmor@gmail.com>
2023-05-26 16:08:44 +0630

    more optimization

--- ch11/word2/word.go
+++ ch11/word2/word.go
@@ -4,7 +4,7 @@ package word
 import "unicode"
 
 func IsPalindrome(s string) bool {
-	var letters []rune
+	letters := make([]rune, 0, len(s))
 	for _, r := range s {
 		if unicode.IsLetter(r) {
 			letters = append(letters, unicode.ToLower(r))
